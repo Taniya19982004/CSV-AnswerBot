738e101c9a2cd350cd42f01573d73f7ca919cfd4
Taniya19982004 <22cse121@gweca.ac.in>
2026-02-12 13:20:50 +0530

    change in some prompt

--- chatbot_code.py
+++ chatbot_code.py
@@ -39,7 +39,7 @@ def create_qa_chain():
 
     prompt_template = """Given the following context and a question, generate an answer based on this context only.
     In the answer try to provide as much text as possible from "response" section in the source document context without making much changes.
-    If the answer is not found in the context, kindly state "I don't know." Don't try to make up an answer.
+    If the answer is not found in the context, kindly state "I don't know." Don't try to make up an answer.Do not significantly modify the original wording.
 
     CONTEXT: {context}
 
